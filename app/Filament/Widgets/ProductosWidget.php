5596258af107b78e5e96892c253baf36edc162a7
YenerArismendi <yenerandres22@gmail.com>
2025-09-07 19:12:41 -0500

    Cambios en lo visual

--- app/Filament/Widgets/ProductosWidget.php
+++ app/Filament/Widgets/ProductosWidget.php
@@ -1,23 +1,23 @@
 <?php
-
-namespace App\Filament\Widgets;
-
-use Filament\Widgets\Widget;
-use App\Models\Article;
-
-class ProductosWidget extends Widget
-{
-    protected static string $view = 'filament.widgets.productos-widget';
-
-    // Listener para refrescar cuando cambie la bodega
-    protected $listeners = ['bodegaChanged' => '$refresh'];
-
-    public function getData(): array
-    {
-        $bodegaId = auth()->user()->active_bodega_id;
-
-        return [
-            'total' => Article::where('bodega_id', $bodegaId)->count(),
-        ];
-    }
-}
+//
+//namespace App\Filament\Widgets;
+//
+//use Filament\Widgets\Widget;
+//use App\Models\Article;
+//
+//class ProductosWidget extends Widget
+//{
+//    protected static string $view = 'filament.widgets.productos-widget';
+//
+//    // Listener para refrescar cuando cambie la bodega
+//    protected $listeners = ['bodegaChanged' => '$refresh'];
+//
+//    public function getData(): array
+//    {
+//        $bodegaId = auth()->user()->active_bodega_id;
+//
+//        return [
+//            'total' => Article::where('bodega_id', $bodegaId)->count(),
+//        ];
+//    }
+//}
