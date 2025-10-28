61fdd6fb6d33f35683f15a0907b40bdafe0a75b8
LFMG1993 <lfmontes662@soy.sena.edu.co>
2025-10-28 00:26:40 -0500

    migraciones

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
@@ -13,17 +14,19 @@ class UserSeeder extends Seeder
     {
         // Crear el rol de 'admin' si no existe (es seguro ejecutarlo de nuevo)
         $adminRole = Role::firstOrCreate(['name' => 'admin', 'guard_name' => 'web']);
+        $superAdminRole = Role::firstOrCreate(['name' => 'super_admin', 'guard_name' => 'web']);
 
         // Crear el usuario administrador si no existe
         $adminUser = User::firstOrCreate(
             ['email' => 'admin@hollywood.com'],
             [
-                'name' => 'admin',
+                'name' => 'Super Admin',
                 'password' => Hash::make('password'), // ¡Cambiar en producción!
             ]
         );
 
         // Asignar el rol de 'admin' al usuario
-        $adminUser->assignRole($adminRole);
+        $adminUser->assignRole($superAdminRole);
+        $superAdminRole->syncPermissions(\Spatie\Permission\Models\Permission::all());
     }
 }
