2091e7af2e57446b66888a4fe3d8018ff02bf69f
LFMG1993 <lfmontes662@soy.sena.edu.co>
2025-10-30 21:22:53 -0500

    server

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
+            ->path('admin')
             ->authGuard('web')
             ->login(\App\Filament\Pages\Auth\Login::class)
             ->colors([
